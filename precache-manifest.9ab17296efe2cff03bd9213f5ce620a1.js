d5fc8bba16e7de8fa7e76b752a175d437583935e
Jessiecodes <Jessak1202@gmail.com>
2020-09-18 13:34:19 -0400

    Updates

---
+++ precache-manifest.9ab17296efe2cff03bd9213f5ce620a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f940295c7ad1405eb31c517febefa6db",
+    "url": "/reactaxios/index.html"
+  },
+  {
+    "revision": "c927076e6578f06bbea1",
+    "url": "/reactaxios/static/css/main.9a9afc6c.chunk.css"
+  },
+  {
+    "revision": "1478ec370368672c81d2",
+    "url": "/reactaxios/static/js/2.12a73740.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactaxios/static/js/2.12a73740.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c927076e6578f06bbea1",
+    "url": "/reactaxios/static/js/main.63c9e551.chunk.js"
+  },
+  {
+    "revision": "914b95ce30f5cc06dd97",
+    "url": "/reactaxios/static/js/runtime-main.66fda0f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactaxios/static/media/logo.5d5d9eef.svg"
+  }
+]);
